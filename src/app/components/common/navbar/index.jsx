772a072aef721b9cfd980ef8404d14972b2c8959
Abdullahi Alimi <96649267+abdvllJSX@users.noreply.github.com>
2025-03-22 21:16:02 +0100

    Fixing conflicts

--- src/app/components/common/navbar/index.jsx
+++ src/app/components/common/navbar/index.jsx
@@ -54,76 +54,10 @@ const Index = () => {
     { label: "Home", link: "/" },
     { label: "About us", link: "/about" },
     { label: "Services", link: "/service" },
-    { label: "Blog", link: "/blogs" },
+    { label: "Blog", link: "#" },
     { label: "Customer Stories", link: "/stories" },
   ];
 
-<<<<<<< HEAD
-    // Scroll effect to change background color
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {  // You can adjust the scroll threshold
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const navItems = [
-        { label: 'Home', link: '/' },
-        { label: 'About us', link: '/about' },
-        { label: 'Services', link: '/service' },
-        { label: 'Blog', link: '#' },
-        { label: 'Customer Stories', link: '/stories' }
-    ];
-
-    return (
-        <nav className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${isScrolled || IsMessageBroker || IsMicroService ? 'bg-black' : 'bg-transparent'}`}> {/* Dynamic class */}
-            <MaxContainer>
-                <div className="flex items-center px-[3rem] sm:px-[4rem] py-[1.5rem] sm:py-[2.5rem] justify-between">
-                    <div className="sm:flex sm:items-center">
-                        <Link href='/'>
-                            <img src="/ace_logo.png" className="w-[7.6rem] relative z-50 sm:w-[10rem] sm:mr-[10rem]" alt="logo" />
-                        </Link>
-                        <div className={`fixed sm:static z-20 w-[100vw] sm:w-auto h-[100vh] sm:h-auto  sm:bg-transparent bg-black inset-0 sm:inset-auto px-[2rem] sm:px-0 transition-all duration-500 ease-[cubic-bezier(0.65, 0, 0.35, 1)] ${open ? 'translate-x-0' : 'translate-x-[100%] sm:translate-x-0'}`}>
-                            <ul className="mt-[7rem] sm:mt-0 sm:flex">
-                                {navItems.map((item, i) => (
-                                    <NavItem
-                                        item={item}
-                                        setOpen={setOpen}
-                                        openService={openService}
-                                        setOpenService={setOpenService}
-                                        id={i}
-                                        key={i}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    {openService && <Service setOpenService={setOpenService} />}
-                    <div className="flex items-center gap-x-[3rem]">
-                        <Link href='/contact-us'>
-                            <button className="font-bold rounded-[.5rem] text-[1rem] sm:text-[1.3rem] sm:border text-text-100 sm:text-white bg-white sm:bg-transparent px-[1.2rem] sm:px-[2.3rem] py-[.7rem]">
-                                Contact us
-                            </button>
-                        </Link>
-                        <div className={`overflow-hidden transition-all duration-500 ease-out flex flex-col items-center justify-between h-[1.7rem] relative z-20 sm:hidden ${open ? 'w-[3rem]' : 'w-[2.5rem]'}`} onClick={toggleNav}>
-                            <div className={`h-[.18rem] bg-white w-[100%] origin-right transition-all duration-500 ease-out ${open ? '-rotate-45 w-[2.7rem]' : ''}`}></div>
-                            <div className={`h-[.18rem] bg-white w-[100%] origin-right transition-all duration-500 ease-out ${open ? 'translate-x-[8rem] translate-y-[8rem]' : ''}`}></div>
-                            <div className={`h-[.18rem] bg-white w-[100%] origin-right transition-all duration-500 ease-out ${open ? 'rotate-45 w-[2.7rem]' : ''}`}></div>
-                        </div>
-                    </div>
-                </div>
-            </MaxContainer>
-        </nav>
-    );
-=======
   return (
     <nav
       className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
@@ -197,7 +131,6 @@ const Index = () => {
       </MaxContainer>
     </nav>
   );
->>>>>>> 350b71609a302b709046faf04647d0fdaed98838
 };
 
 export default Index;
