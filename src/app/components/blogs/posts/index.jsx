2e35a1af49b460ef60dc57e23223b0d92df0504b
Abdullahi Alimi <abdullahi.alimi@nematix.com>
2025-08-10 12:08:25 +0100

    AADED NEW BLOG POST

--- src/app/components/blogs/posts/index.jsx
+++ src/app/components/blogs/posts/index.jsx
@@ -20,11 +20,6 @@ const Index = () => {
             header: "Nginx Consulting: Building Resilient, High-Performance Web Infrastructure",
             text: "In the age of digital transformation, organizations face escalating pressure to deliver lightning-fast"
         },
-        {
-            image: "/blogs/tanzu.jpg",
-            header: "Tanzu RabbitMQ Licensing: The Complete Guide to Enterprise Messaging Value",
-            text: "In the world of enterprise IT, the right message broker can be the difference between scalable success and exponential headaches",
-        }
     ]
     return (
         <section className="bg-white px-[3rem] sm:px-[10rem] py-[5rem] text-[#232323]">
